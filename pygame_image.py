0eafd29515c80d24f32a4b1505e315700150610b
Ryousei <c0a2422726@edu.teu.ac.jp>
2026-04-14 16:18:51 +0900

    練習10:こうかとんが動く

--- pygame_image.py
+++ pygame_image.py
@@ -14,16 +14,28 @@ def main():
     flip_img = pg.image.load("fig/pg_bg.jpg")
     flip_img = pg.transform.flip(flip_img, True, False)
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_list = pg.key.get_pressed()
+        if key_list[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        if key_list[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        if key_list[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
+        if key_list[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
         x = tmr%3200
         screen.blit(bg_img, [-x, 0]) #練習2
         screen.blit(flip_img, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, kk_rct)
+
         pg.display.update()
         tmr += 1        
         clock.tick(200)
